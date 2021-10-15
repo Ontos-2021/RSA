cc4b80563515f47c02d250c7eb24369e1e245aa5
_Ontos_ <josemercado.musica@gmail.com>
2021-10-15 01:01:28 -0300

    Agregué instrucciones. Creé función para crear lista con números primos.

--- PRIMOS/primos.py
+++ PRIMOS/primos.py
@@ -1,4 +1,30 @@
 # Acá irán las funciones relacionadas a números primos
 
-def primos():
-    return "Hola! Soy un número primo"
+def crear_primos():
+    primos = [2]
+    n = 3
+    for i in range(1, 100000):
+
+        # Verificar si n es divisible por alguno de los primos ya encontrados
+
+        es_primo = True
+
+        for primo in primos:
+
+            # En este caso n no es divisible por el primo actual
+            if n % primo == 0:
+
+                es_primo = False
+                # print(f"n: {n}, primo = {es_primo}. Número primo por el cual se divide: {primo}")
+                break
+
+            # rint(f"n: {n}, primo = {es_primo}. Número primo por el cual se divide: {primo}")
+
+        if es_primo:
+
+            primos.append(n)
+
+        n += 2
+
+    print(f"Estos son los números primos: {primos}")
+
--- main.py
+++ main.py
@@ -1,3 +1,33 @@
-from MCD.MCD import *
+# Acá estará el main
 
-print(f"Este es la suma entre los dos números: {mcd(7, 13)}")
+""" Paso 1
+Primero, hay que elegir dós números primos aleatorios
+En este caso, P y Q
+
+P = 83
+Q = 97 """
+
+""" Paso 2
+Una vez que tenemos P y Q, podemos determinar N.
+La ecuación es N = P * Q
+
+N = P * Q = (83) * (97) = 8051 """
+
+""" Paso 3
+Luego hay que determinar pi(n).
+El cual es
+pi(n) = (p-1)(q-1) = (83-1)(97-1) = (82)(96) = 7872 """
+
+""" Paso 4
+Luego hay que encontrar o elegir 'e'.
+El cual tiene dos condiciones
+1 < e
+mcd(e, pi(n))= = 1 """
+
+""" Llave pública = (e, n)
+    Llave privada = (d, n)
+"""
+
+from PRIMOS.primos import *
+
+crear_primos()
